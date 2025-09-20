b0497108180ef7da718f8fc76fa1ff4a986c51e6
kaustubhbth <91954071+kaustubhbth@users.noreply.github.com>
2025-09-21 02:25:46 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,6 +1,4 @@
-def main():
-    name = input("Enter your name: ").strip()
-    age = input("Enter your age: ").strip()
-    print(f"Hello, {name}! You are {age} years old. Welcome! 😊")
-if __name__ == "__main__":
-    main()
+name = input("Enter your name: ").strip()
+age = input("Enter your age: ").strip()
+print(f"Hello, {name}! You are {age} years old. Welcome! 😊")
+
