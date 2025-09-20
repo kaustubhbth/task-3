0f6fb97ed81b4789466046395e4cef624b5f553e
kaustubhbth <91954071+kaustubhbth@users.noreply.github.com>
2025-09-21 02:24:59 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,6 @@
+def main():
+    name = input("Enter your name: ").strip()
+    age = input("Enter your age: ").strip()
+    print(f"Hello, {name}! You are {age} years old. Welcome! 😊")
+if __name__ == "__main__":
+    main()
